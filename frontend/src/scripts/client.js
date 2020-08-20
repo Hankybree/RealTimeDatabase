aec16fb2e0b3d1c61b3b5657c4db98ee72d9a7b4
Henrik Frank <frank.havsvinge@gmail.com>
2020-08-20 11:40:46 +0200

    Cleaned up code and broken out if-else-if to function to make code more readable

--- frontend/src/scripts/client.js
+++ frontend/src/scripts/client.js
@@ -12,44 +12,31 @@ export const client = {
         }
 
         context.state.socket.onmessage = (message) => {
-            
+
             let data = JSON.parse(message.data)
 
             console.log(data)
 
-            if (data.status === 1) {
-                context.commit('setImages', data.data)
-            } else if (data.status === 2 || data.status === 3 || data.status === 4) {
-
-                if(data.status === 2) {
-                    client.like(context, data)
-                } else if (data.status === 3) {
-                    client.comment(context, data)
-                } else if (data.status === 4) {
-                    client.deleteComment(context, data)
-                }
-
-                // for (let i = 0; i < context.state.images.length; i++) {
-                //     for (let j = 0; j < data.data.length; j++) {
-                //         if (context.state.images[i].imageId === data.data[j].imageId) {
-                //             if(data.status === 2) {
-                //                 Vue.set(context.state.images[i], 'likes', data.data[j].likes)
-                //             } else if (data.status === 3 || data.status === 4) {
-                //                 Vue.set(context.state.images[i], 'comments', data.data[j].comments)
-                //             }
-                //         }
-                //     }
-                // }
-            }
-            
+            client.checkStatus(context, data)
         }
 
         context.state.socket.onclose = () => {
             console.log('Disconnected from server')
         }
     },
+    checkStatus(context, data) {
+        if (data.status === 1) {
+            context.commit('setImages', data.data)
+        } else if (data.status === 2) {
+            client.like(context, data)
+        } else if (data.status === 3) {
+            client.comment(context, data)
+        } else if (data.status === 4) {
+            client.deleteComment(context, data)
+        }
+    },
     like(context, data) {
-        if(data.isLiking) {
+        if (data.isLiking) {
             context.state.images.find(image => image.imageId === data.likeImageId).likes.push(data.likeUserId)
         } else {
             let likeArray = context.state.images.find(image => image.imageId === data.likeImageId).likes
@@ -60,7 +47,6 @@ export const client = {
         context.state.images.find(image => image.imageId === data.commentImageId).comments.push({ commentId: data.commentId, commentUserId: data.commentUserId, commentMessage: data.commentMessage })
     },
     deleteComment(context, data) {
-        // console.log(commentArray.find(comment => comment.commentId === data.commentId))
         let commentArray = context.state.images.find(image => image.imageId === data.imageId).comments
         commentArray.splice(commentArray.indexOf(commentArray.find(comment => comment.commentId === data.commentId)), 1)
     }
